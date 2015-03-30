01a48eab5bba1d7586c1037a61b38234796d3f69
Callum Locke <callum.locke@gmail.com>
2015-03-30 15:15:19 +0100

    remove ondomready (DOMContentLoaded is provided by polyfill.io)

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -2,7 +2,7 @@
 
 var oHoverable = require('o-hoverable');
 
-onDomReady(function () {
+document.addEventListener('DOMContentLoaded', function () {
 
   // Automatically fix hover effects to work on touch devices
   oHoverable.init();
