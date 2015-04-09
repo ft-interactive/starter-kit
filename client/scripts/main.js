05de1a9320659794b9ed26af276145f935c1a1da
Callum Locke <callum.locke@gmail.com>
2015-04-09 20:46:20 +0100

    add babel polyfill option

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// Uncomment this if you need to use Set, Map, generators etc.
+// import 'babel/polyfill';
+
 import oHoverable from 'o-hoverable';
 
 document.addEventListener('DOMContentLoaded', function () {
