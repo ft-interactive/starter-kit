2fd0a545ea1cf3fbfd17c69651be1df62a538b9a
Callum Locke <callum.locke@gmail.com>
2015-05-14 12:55:27 +0100

    remove babel polyfill line

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -1,12 +1,8 @@
 'use strict';
 
-// Uncomment this if you need to use Set, Map, generators etc.
-// import 'babel/polyfill';
-
 import oHoverable from 'o-hoverable';
 
 document.addEventListener('DOMContentLoaded', function () {
   oHoverable.init(); // makes hover effects work on touch devices
-
 
 });
