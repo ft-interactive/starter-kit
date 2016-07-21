2dd5633b6fd175c89b1cb54663bd88ab4a995c01
Kavanagh, Luke <ltkavanagh@gmail.com>
2016-07-21 21:49:17 +0100

    Comment about comments

--- config/flags.js
+++ config/flags.js
@@ -8,17 +8,26 @@ export default _ => ({
   ads: true,
   onwardjourney: true,
   shareButtons: true,
+  header: true,
+  footer: true,
 
   /*
+    NOTE ABOUT COMMENTS:
+
+    1)
     For comments to work locally you cannot run on
     localhost, you need an ft.com hostname
     Add the follow entry to the end of the hosts file
 
     127.0.0.1 ig-local.ft.com
+
+    2)
+    In order for comments to work the page needs a UUID.
+    If you are yet to create an article UUID the following test UUID
+    will be used
+
+    3a499586-b2e0-11e4-a058-00144feab7de
+
   */
   comments: true,
-
-  header: true,
-  footer: true,
-
 })
