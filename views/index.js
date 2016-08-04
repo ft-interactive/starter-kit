1e5ee9fc21fd8597e9495520ee8e60bfa173f2d9
Ændrew Rininsland <andrew.rininsland@ft.com>
2016-08-04 10:13:43 +0100

    Updates references of ig-article to g-ui.

--- views/index.js
+++ views/index.js
@@ -1,11 +1,11 @@
 /**
  * Starter Kit Nunjucks config
  *
- * This pulls in templates and filters from ft-interactive/ig-article and
+ * This pulls in templates and filters from ft-interactive/g-ui and
  * then adds any extra ones added to views/ and views/filters, respectively.
  */
 
-import { templates } from 'ig-article/lib';
+import { templates } from 'g-ui/lib';
 
 export function configure() {
   delete require.cache[require.resolve('./filters/index')];
