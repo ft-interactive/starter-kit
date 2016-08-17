f3c9796cf4d6a503365b634ca1f0996890723d7e
Kavanagh, Luke <ltkavanagh@gmail.com>
2016-08-17 18:05:39 +0100

    Add Nunjucks utils
    * Allow debugging the template context
    * Adds a global now() function to all templates.
    * JSON and JS score elementpt filters
    * Better time formatting filters
    * Template the year in the copyright notice - Fixes #10

--- views/filters/index.js
+++ views/filters/index.js
@@ -1,45 +1,100 @@
 import markdownIt from 'markdown-it';
 import removeMarkdown from 'remove-markdown';
+import { utcFormat } from 'd3-time-format';
 import nunjucks from 'nunjucks';
 
-const days  = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
-                  'September', 'October', 'November', 'December'];
+const SafeString = nunjucks.runtime.SafeString;
+
+const formatterCache = new Map();
+const defaultFTDateFormat = '%A, %-e %B %Y';
+
+export function isotime(date) {
+  if (!date) {
+    return '';
+  } else if (!(date instanceof Date)) {
+    return date;
+  }
+
+  return date.toISOString();
+}
+
+// strftime format docs: https://github.com/d3/d3-time-format
+export function strftime(date, format = defaultFTDateFormat) {
+  if (!date) {
+    return '';
+  } else if (!(date instanceof Date)) {
+    return date;
+  }
+
+  if (formatterCache.has(format)) {
+    return formatterCache.get(format)(date);
+  }
+
+  const fm = utcFormat(format);
+  formatterCache.set(format, fm);
+  return fm(date);
+}
 
 export function ftdate(d) {
-  const day = days[d.getUTCDay()];
-  const month = months[d.getUTCMonth()];
-  return !d ? '' : `${day}, ${d.getUTCDate()} ${month}, ${d.getUTCFullYear()}`;
+  return strftime(d);
 }
 
 const markdown = markdownIt({
   html: true,
   linkify: true,
   typographer: true,
-  breaks: true
+  breaks: true,
 });
 
 export function md(str, inline) {
-  return !str ? '' : new nunjucks.runtime.SafeString(inline ? markdown.renderInline(str) : markdown.render(str));
+  return !str ? '' :
+    new SafeString(inline ? markdown.renderInline(str) : markdown.render(str));
 }
 
-export function plain(str, stripListLeaders=true) {
-  return removeMarkdown(str, {stripListLeaders: stripListLeaders, gfm: true});
+export function plain(str, stripListLeaders = true) {
+  return removeMarkdown(str, { stripListLeaders, gfm: true });
+}
+
+export function json(o, prop) {
+  try {
+    return new SafeString(
+      JSON.stringify(prop ? o[prop] : o).replace(/<\/script/g, '\\x3C/script')
+    );
+  } catch (e) {
+    return '';
+  }
+}
+
+export function inlineScriptElement(o, name) {
+  return new SafeString(
+    `<script>;(function(){window.${name}=${json(o)};}());</script>`
+  );
+}
+
+export function jsonScriptElement(o, id, attr) {
+  const idAttr = id ? ` id="${id}"` : '';
+  return new SafeString(
+    `<script type="application/json"${idAttr} ${attr}>${json(o)}</script>`
+  );
 }
 
 export function encodedJSON(str) {
   try {
-    return encodeURIComponent(JSON.stringify(JSON.parse(str || ''), null, ''));
+    return encodeURIComponent(
+      JSON.stringify(JSON.parse(str || ''), null, '')
+    );
   } catch (e) {
     return '';
   }
 }
 
 export function spoorTrackingPixel(str) {
-  const json = encodedJSON(str.trim());
-  const img = `<img src="https://spoor-api.ft.com/px.gif?data=${json}" height="1" width="1" />`;
-  return new nunjucks.runtime.SafeString(`<!--[if lt IE 9]>
-    ${img}
-    <![endif]-->
-    <noscript data-o-component="o-tracking">${img}</noscript>`);
+  const jsonString = encodedJSON(str.trim());
+  const img = `<img src="https://spoor-api.ft.com/px.gif?data=${jsonString}" height="1" width="1" />`;
+  return new SafeString(
+  `<!--[if lt IE 9]>
+  ${img}
+  <![endif]-->
+  <noscript data-o-component="o-tracking">${img}</noscript>`
+  );
 }
--- views/index.js
+++ views/index.js
@@ -1,17 +1,23 @@
 import nunjucks from 'nunjucks';
-import markdown_tag from 'nunjucks-markdown';
-import markdownIt from 'markdown-it';
+import markdownTag from 'nunjucks-markdown';
 
 export function configure() {
-
   delete require.cache[require.resolve('./filters/index')];
 
   const env = new nunjucks.Environment(
     new nunjucks.FileSystemLoader(['client', 'views'])
   );
 
-  Object.assign(env.filters, require('./filters'));
-  markdown_tag.register(env, env.filters.md);
+  Object.assign(env.filters, require('./filters'));  // eslint-disable-line
+  markdownTag.register(env, env.filters.md);
+  env.globals.now = function now(unixtime) {
+    return unixtime ? Date.now() : new Date();
+  };
+  env.globals.ctx = function ctx(property, outputJSON) {
+    const value = typeof property === 'string' ? this.ctx[property] : this.ctx;
+    const stringify = outputJSON || (typeof property === 'boolean' && property);
+    return stringify ? env.filters.json(value) : value;
+  };
 
   return env;
 }
