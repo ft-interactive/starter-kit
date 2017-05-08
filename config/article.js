42e60f8c353fe7e9ea69bccfd08b08b6cc9f599f
Leach <anna.leach@FT-MW4802.local>
2017-05-08 12:52:47 +0100

    Add new share buttons & fix surrounding layout

--- config/article.js
+++ config/article.js
@@ -29,8 +29,9 @@ export default () => ({ // eslint-disable-line
 
   mainImage: {
     title: '',
-    description: '',
-    url: '',
+    description: 'George Orwell at his typewriter',
+    credit: 'Credit person',
+    url: 'https://image.webservices.ft.com/v1/images/raw/http%3A%2F%2Fcom.ft.imagepublish.prod.s3.amazonaws.com%2Fc4bf0be4-7c15-11e4-a7b8-00144feabdc0?source=ig&fit=scale-down&width=700',
     width: 2048, // ensure correct width
     height: 1152, // ensure correct height
   },
