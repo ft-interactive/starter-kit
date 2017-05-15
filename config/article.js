4e4a363af09015d33b057597f2b9866fbbcaa293
Joanna Kao <joanna.kao@ft.com>
2017-05-15 14:19:23 -0400

    edit comments. use v2 image service url for example image url

--- config/article.js
+++ config/article.js
@@ -32,11 +32,11 @@ export default () => ({ // eslint-disable-line
     description: '',
     credit: '',
 
-    // You can provide a UUID to an image and it was populate everything else
+    // You can provide a UUID to an image and it will populate everything else
     uuid: 'c4bf0be4-7c15-11e4-a7b8-00144feabdc0',
 
-    // You can also provide a URL
-    // url: 'https://image.webservices.ft.com/v1/images/raw/http%3A%2F%2Fcom.ft.imagepublish.prod.s3.amazonaws.com%2Fc4bf0be4-7c15-11e4-a7b8-00144feabdc0?source=ig&fit=scale-down&width=700',
+    // Or you can provide a URL to the image
+    // url: 'https://www.ft.com/__origami/service/image/v2/images/raw/ftcms%3Ac4bf0be4-7c15-11e4-a7b8-00144feabdc0?source=ig&fit=scale-down&width=700',
 
     width: 2048, // ensure correct width
     height: 1152, // ensure correct height
