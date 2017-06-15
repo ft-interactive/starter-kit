32e7f9e139f39549b9116365b59b07e0b475dfcb
Ændrew Rininsland <aendrew@aendrew.com>
2017-06-15 11:17:48 +0100

    ✨ Adds ability to specify image UUID to set main article image (#95)
    * :sparkles: Adds ability to specify an image UUID to set main article image
    Resolves #82, resolve #66
    
    * edit comments. use v2 image service url for example image url
    
    * Removing width/height properties as I don't think they're used anywhere.

--- views/filters/index.js
+++ views/filters/index.js
@@ -98,3 +98,13 @@ export function spoorTrackingPixel(str) {
   <noscript data-o-component="o-tracking">${img}</noscript>`,
   );
 }
+
+export function imageUUID(uuid) {
+  return `https://www.ft.com/__origami/service/image/v2/images/raw/ftcms%3A${uuid}?source=ig`;
+}
+
+export function getMainImage(img) {
+  if (Object.prototype.hasOwnProperty.call(img, 'uuid')) return imageUUID(img.uuid);
+  else if (Object.prototype.hasOwnProperty.call(img, 'url')) return img.url;
+  return '';
+}
