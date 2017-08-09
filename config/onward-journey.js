50457a6445a0be075c83d3e04e818ce0aa9bfa83
Callum Locke <callum.locke@gmail.com>
2017-08-09 17:21:32 +0100

    Fix onward journey client code (#127)
    * use v2 UUID by default
    
    * specify v2 in onwardjourney api url

--- config/onward-journey.js
+++ config/onward-journey.js
@@ -1,6 +1,15 @@
+// You can look for "data-concept-id" on any stream page to find its UUID,
+// and then prefix this with 'thing/' to use it below.
+//
+// EXAMPLES:
+//
+// Graphics (Methode list): 'list/graphics'
+// World: 'thing/d8009323-f898-3207-b543-eab4427b7a77'
+// UK: 'thing/98815f9a-0c35-3824-98fb-f134965f56b7'
+
 export default () => ({
   relatedContent: [
     { rows: 1, list: 'list/graphics' },
-    { rows: 1, list: 'thing/MTcw-U2VjdGlvbnM=' },
+    { rows: 1, list: 'thing/d8009323-f898-3207-b543-eab4427b7a77' },
   ],
 });
