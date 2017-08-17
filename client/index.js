65a9ad14343cdf64656a7081c47f3cd689d8514e
Ændrew Rininsland <andrew.rininsland@ft.com>
2017-08-17 14:45:05 +0100

    Adding more documentation to client/index

--- client/index.js
+++ client/index.js
@@ -18,4 +18,12 @@ import './styles.scss';
             - index.js
             - template.html
 
+  If you want to import some data, just import it like normal:
+
+  import myData from './data.csv';
+
+  `myData` will be a string that you can then parse into an object using, for example, d3.csvParse()
+
+  You can import CSV, TSV, TXT, XML and JSON this way! Note, however, that it will increase your
+  bundle size, which may increase the time to first render in some cases!
 */
