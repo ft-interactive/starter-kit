066235ec5e931b4b0f0d1464076ed350f30130a1
Max Harlow <max.harlow@ft.com>
2020-06-05 13:30:50 +0100

    Use new layout components

--- app/app.js
+++ app/app.js
@@ -49,7 +49,9 @@
 
 import React, { useEffect, useState } from 'react';
 import { hot } from 'react-hot-loader/root';
-import Layout from '@financial-times/g-components/layout';
+import ArticleLayout from '@financial-times/g-components/article-layout';
+import { StoryTopper } from '@financial-times/g-components/story-topper';
+import { GridContainer, GridRow, GridChild } from '@financial-times/g-components/grid';
 import { ContextPropType, ContextDefaultProps } from './util/prop-types';
 
 const App = context => {
@@ -72,73 +74,86 @@ const App = context => {
   console.dir(data); // eslint-disable-line no-console
 
   return (
-    <Layout {...context}>
-      <p>
-        Ik kie neġi æpude pōsÞpriskribo, anċ ēg tiel subtegmenÞo. Giga gārði esperǣntigo vi jes. Ċit
-        plēj esceptīnte hu, ōl vola eksploðæ poǽ. Ōīð gh pǽƿjo s&apos;joro pronomeċa, mi paki vice
-        fiksa vir. Trǣ kibi multa ok, sur ðū īnfāno kæŭze. Om ene modō sekvanta proksimumecō, ānÞ sh
-        tiele hiper defīnītive.
-      </p>
+    <ArticleLayout {...context}>
+      <GridContainer>
+        <GridRow>
+          <GridChild colspan="12 S11 Scenter M10 L9 XL8">
+            <StoryTopper {...context} />
+          </GridChild>
+        </GridRow>
+      </GridContainer>
+      <GridContainer>
+        <GridRow>
+          <GridChild colspan="12 S11 Scenter M10 L9 XL8">
+            <p>
+              Ik kie neġi æpude pōsÞpriskribo, anċ ēg tiel subtegmenÞo. Giga gārði esperǣntigo vi jes. Ċit
+              plēj esceptīnte hu, ōl vola eksploðæ poǽ. Ōīð gh pǽƿjo s&apos;joro pronomeċa, mi paki vice
+              fiksa vir. Trǣ kibi multa ok, sur ðū īnfāno kæŭze. Om ene modō sekvanta proksimumecō, ānÞ sh
+              tiele hiper defīnītive.
+            </p>
 
-      <p>
-        Nk sola ēsperanÞiġo obl, mulÞō ipsilono nēdifīnita ien ed. Trīliono kōmpleksa co mil, kī āġā
-        farī onin triǣnġulo. I eŭro postā eksteren eƿd, ig nūna viro īnstruītulo anc, gē īsm mēze
-        ƿuancilo kīlometro. Ts rīlāte nekuÞima ðārǽlȝæjdō plue.
-      </p>
+            <p>
+              Nk sola ēsperanÞiġo obl, mulÞō ipsilono nēdifīnita ien ed. Trīliono kōmpleksa co mil, kī āġā
+              farī onin triǣnġulo. I eŭro postā eksteren eƿd, ig nūna viro īnstruītulo anc, gē īsm mēze
+              ƿuancilo kīlometro. Ts rīlāte nekuÞima ðārǽlȝæjdō plue.
+            </p>
 
-      <p>
-        Sēmi rolfinaĵo far nv, sūpēr sċivolema ǽfgænistāno kaj ej. LēÞēri frǽzmelodio eg plue,
-        kiomæs sælutfrāzo ig hej. Korūso ekskluzive ǽnÞǣŭprīskrībo ȝo ena, ilī hā duonvokalō
-        sekviƿȝēro. Lo esti adjēktivo duǣ, san simil multekostā iƿfinitīvo ēj. Is pakī rolfinaĵō
-        sāt, kūƿ æl jaro sæmtempē, milo īmperǣtīvo ba ƿiǣ. Malebliġi esperantiġo pri rē, dum et
-        duōno grupo sekstiliono.
-      </p>
+            <p>
+              Sēmi rolfinaĵo far nv, sūpēr sċivolema ǽfgænistāno kaj ej. LēÞēri frǽzmelodio eg plue,
+              kiomæs sælutfrāzo ig hej. Korūso ekskluzive ǽnÞǣŭprīskrībo ȝo ena, ilī hā duonvokalō
+              sekviƿȝēro. Lo esti adjēktivo duǣ, san simil multekostā iƿfinitīvo ēj. Is pakī rolfinaĵō
+              sāt, kūƿ æl jaro sæmtempē, milo īmperǣtīvo ba ƿiǣ. Malebliġi esperantiġo pri rē, dum et
+              duōno grupo sekstiliono.
+            </p>
 
-      <p>
-        Fri ok ðekǣ hūrā, ho resÞi fīnāĵvorto substǽnÞivā ǽjn. Oz ūƿ&apos; mēġā okej&apos;
-        perlæbori, ēl ǣŭ pobo līgvokālo, tio esÞiel finnlanðo il. Ad oƿī ðeko ālternaÞivǣ, i kvær
-        fuÞuro tabelvorto iēl, veo mo mālpli alimǣnierē. Movi ilīard anÞāŭpǣrto īli om, sorī
-        popolnomo prēpozīcīō ul tiē, prā mīria kurÞā praaƿtaŭhieraŭ lo.
-      </p>
+            <p>
+              Fri ok ðekǣ hūrā, ho resÞi fīnāĵvorto substǽnÞivā ǽjn. Oz ūƿ&apos; mēġā okej&apos;
+              perlæbori, ēl ǣŭ pobo līgvokālo, tio esÞiel finnlanðo il. Ad oƿī ðeko ālternaÞivǣ, i kvær
+              fuÞuro tabelvorto iēl, veo mo mālpli alimǣnierē. Movi ilīard anÞāŭpǣrto īli om, sorī
+              popolnomo prēpozīcīō ul tiē, prā mīria kurÞā praaƿtaŭhieraŭ lo.
+            </p>
 
-      <p>
-        Prōto rōlfīnaĵo posÞpostmorgæŭ vol je, ve kelkē inkluzive siƿ. Ōmetr ġræðo ipsilōno ðū ǽto,
-        iġi negi dēcilionō esperantigo æc, il unuo ulÞra aŭ. Milo fini iufoje dis be, ænt ēl hēkto
-        hǣlÞōsÞreko, hot ab mēġā sūbfrǣzo. Rō āpuð kiloġrāmo mal, ties kromakċento iƿÞerogatīvo ot
-        nur. Kunskribo profitænÞo prǽantæŭlǽsÞa ǣs plue, tǣgō tiūdirekten ni neā.
-      </p>
+            <p>
+              Prōto rōlfīnaĵo posÞpostmorgæŭ vol je, ve kelkē inkluzive siƿ. Ōmetr ġræðo ipsilōno ðū ǽto,
+              iġi negi dēcilionō esperantigo æc, il unuo ulÞra aŭ. Milo fini iufoje dis be, ænt ēl hēkto
+              hǣlÞōsÞreko, hot ab mēġā sūbfrǣzo. Rō āpuð kiloġrāmo mal, ties kromakċento iƿÞerogatīvo ot
+              nur. Kunskribo profitænÞo prǽantæŭlǽsÞa ǣs plue, tǣgō tiūdirekten ni neā.
+            </p>
 
-      <p>
-        U Þrā hodiæŭa dupunkto proƿōmecǽ, aliām difinǣ pentēkosto āb frī. Ist it kūne dēcīliono
-        moƿtrovorÞo. Huræ sēkvinbero prepoziciæĵo jh iam, mīnca fontōj renkōntēƿ ƿe dev. Nǽŭ vǣtto
-        pri ge. Hurā franjo sēn em.
-      </p>
+            <p>
+              U Þrā hodiæŭa dupunkto proƿōmecǽ, aliām difinǣ pentēkosto āb frī. Ist it kūne dēcīliono
+              moƿtrovorÞo. Huræ sēkvinbero prepoziciæĵo jh iam, mīnca fontōj renkōntēƿ ƿe dev. Nǽŭ vǣtto
+              pri ge. Hurā franjo sēn em.
+            </p>
 
-      <p>
-        Egālo nenīo kapæbl ej sep. Uƿt ed pægo sepen faras, ia perē mālsuperǣ mīs. Ǽt vēō aviō
-        kuƿīgi preÞerito. Kiǣ us vendo kiomæs sezōnonōmo, for si vidalvīde punkÞōkomo geiƿsÞrūisto.
-      </p>
+            <p>
+              Egālo nenīo kapæbl ej sep. Uƿt ed pægo sepen faras, ia perē mālsuperǣ mīs. Ǽt vēō aviō
+              kuƿīgi preÞerito. Kiǣ us vendo kiomæs sezōnonōmo, for si vidalvīde punkÞōkomo geiƿsÞrūisto.
+            </p>
 
-      <p>
-        Land vēaði bv īng, hēlpi alīġi dividostrēkō hāv jo. Dek supēr ǽntǣŭtægmēzo ū. Oj mini ǽrkī
-        sǽmideǽno fin, eg plej nēnī āga, tīmī disskribædō sh fri. Hiper rēalǣ fonÞoj Þs ahǣ.
-        Deċīmala līternomo koƿdicioƿalo ōÞ ses, enð nj pæko reciproke.
-      </p>
+            <p>
+              Land vēaði bv īng, hēlpi alīġi dividostrēkō hāv jo. Dek supēr ǽntǣŭtægmēzo ū. Oj mini ǽrkī
+              sǽmideǽno fin, eg plej nēnī āga, tīmī disskribædō sh fri. Hiper rēalǣ fonÞoj Þs ahǣ.
+              Deċīmala līternomo koƿdicioƿalo ōÞ ses, enð nj pæko reciproke.
+            </p>
 
-      <p>
-        Atō iz velā disðē, ālīo ōkej&apos; neoficiālæ for al, āliom ælīel kioma unū kv. Intere
-        nēniæĵō eksteren mia is, pako mīloj demanðosignō vir je, grupǽ kromakcento iu meƿ. Ido Þiǽl
-        kōmbi fræto po, ko iēs vǽto ġlotā lǽndonomo, he vīc ēkōo ƿanō. Anƿo sekviƿȝero uk tet, us
-        mekæo iomete træ. Int co onjo finnlæƿðo subjunkċiō, kaj faka eblecō mīnimumē ōƿ. Ore verba
-        ðuonhoro komplēksā il, hierāŭæ propōzicio ÞīudīrēkÞen iz sur.
-      </p>
+            <p>
+              Atō iz velā disðē, ālīo ōkej&apos; neoficiālæ for al, āliom ælīel kioma unū kv. Intere
+              nēniæĵō eksteren mia is, pako mīloj demanðosignō vir je, grupǽ kromakcento iu meƿ. Ido Þiǽl
+              kōmbi fræto po, ko iēs vǽto ġlotā lǽndonomo, he vīc ēkōo ƿanō. Anƿo sekviƿȝero uk tet, us
+              mekæo iomete træ. Int co onjo finnlæƿðo subjunkċiō, kaj faka eblecō mīnimumē ōƿ. Ore verba
+              ðuonhoro komplēksā il, hierāŭæ propōzicio ÞīudīrēkÞen iz sur.
+            </p>
 
-      <p>
-        Mæl denta sūȝstǣnÞivo bv, ēhe stif armo duūmæ ōp. Ec Þet pluso traigi. Ē vēla lǣstæ
-        fiƿǽĵvorto kūn. Jesī kiomas duondifinǽ hej he, agæ færās malloƿġīgō go. Tripunkto
-        reciprōkeċo op ǽġā, eliġi eŭro postmorgǣŭ ul anc.
-      </p>
-    </Layout>
+            <p>
+              Mæl denta sūȝstǣnÞivo bv, ēhe stif armo duūmæ ōp. Ec Þet pluso traigi. Ē vēla lǣstæ
+              fiƿǽĵvorto kūn. Jesī kiomas duondifinǽ hej he, agæ færās malloƿġīgō go. Tripunkto
+              reciprōkeċo op ǽġā, eliġi eŭro postmorgǣŭ ul anc.
+            </p>
+          </GridChild>
+        </GridRow>
+      </GridContainer>
+    </ArticleLayout>
   );
 };
 
