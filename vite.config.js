dbb18edb1d5866a33842ff95ddaf7842e389e5cf
danclark18 <daniel.clark@ft.com>
2023-10-19 17:27:32 +0100

    tweaking vite config to flag production

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,32 @@
 import { defineConfig } from 'vite';
 import generateFile from 'vite-plugin-generate-file';
 import react from '@vitejs/plugin-react';
-import getContext from './config/index.js';
 import { ViteEjsPlugin } from 'vite-plugin-ejs';
 import dsv from '@rollup/plugin-dsv';
-
-const context = await getContext();
+import getContext from './config/index.js';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  plugins: [
-    ViteEjsPlugin(context, {
-      ejs: {
-        _with: false,
-        localsName: 'context',
-      },
-    }),
-    generateFile([
-      {
-        type: 'json',
-        output: './context.json',
-        data: context,
-      },
-    ]),
-    react(),
-    dsv(),
-  ],
+export default defineConfig(async ({ mode }) => {
+  const context = await getContext(mode);
+
+  return {
+    base: './',
+    plugins: [
+      ViteEjsPlugin(context, {
+        ejs: {
+          _with: false,
+          localsName: 'context',
+        },
+      }),
+      generateFile([
+        {
+          type: 'json',
+          output: './context.json',
+          data: context,
+        },
+      ]),
+      react(),
+      dsv(),
+    ],
+  };
 });
