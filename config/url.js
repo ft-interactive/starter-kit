04b1de0a483021e2d2d0c551b9c41c81892aaa73
Peter Andringa <peter.andringa@ft.com>
2024-01-25 10:31:31 +0000

    fix base url logic

--- config/url.js
+++ config/url.js
@@ -1,26 +1,22 @@
 // eslint-disable-next-line no-unused-vars
-export default function getUrl(articleUrl, environment = 'development') {
-  if (process.env.CIRCLECI) {
-    // These env variables come from CircleCI
-    const project = process.env.CIRCLE_PROJECT_REPONAME;
-    const branch = process.env.CIRCLE_BRANCH;
-    if (!project || !branch)
-      throw new Error('Detected CIRCLECI env, but CIRCLE_REPO_NAME and CIRCLE_BRANCH are missing.');
-
-    // This env variable is used by g-deploy
-    const bucket = process.env.BUCKET_NAME_PROD;
-    if (!bucket) throw new Error('The BUCKET_NAME_PROD variable (used by g-deploy) is required.');
-
-    if (articleUrl && branch === 'main') {
-      // Use the canonical URL from article.js if it exists
-      // (usually https://ig.ft.com/some-project)
+export default function getUrl(
+  articleUrl,
+  environment = 'development',
+  org = process.env.CIRCLE_PROJECT_USERNAME,
+  project = process.env.CIRCLE_PROJECT_REPONAME,
+  branch = process.env.CIRCLE_BRANCH
+) {
+  switch (environment) {
+    case 'production':
+      // Prod builds use the canonical URL
+      // (Usualy https://ig.ft.com/project-name)
       return articleUrl;
-    }
-
-    // Otherwise infer the deployed test URL from the environment
-    return `http://${bucket}.s3-website-eu-west-1.amazonaws.com/v2/ft-interactive/${project}/${branch}/`;
+    case 'preview':
+      // Infer preview builds from the CircleCI environment
+      return `https://ig-preview.in.ft.com/preview/${org}/${project}/${branch}/`;
+    case 'development':
+    default:
+      // Default to a simple root URL (e.g. for 'npm run start' or 'npm run preview')
+      return '/';
   }
-
-  // Default to a simple root URL (e.g. for 'npm run start' or 'npm run preview')
-  return '/';
 }
