635dde48765fedcbb3bdcd63e397d26d28ecf41d
Peter Andringa <peter.j.andringa@gmail.com>
2024-08-19 12:40:24 -0400

    update vike-react to 0.5.3 to get bugfix

--- pages/+htmlAttributes.js
+++ pages/+htmlAttributes.js
@@ -1,13 +1,6 @@
-// This version currently doesn't work, because vite-react has a bug:
-// https://github.com/vikejs/vike-react/issues/82#issuecomment-2286323622
-// export default ({ data: context }) => ({
-//   class: context.pageClasses,
-//   'data-build-time': context.buildTime,
-//   'data-content-id': context.id,
-// });
-
-export default {
-  class: 'core',
-  'data-build-time': new Date().toISOString(),
-  'data-content-id': 'paste-temp-uuid-value-here',
-};
+// This object is turned into attrs on the <html> tag
+export default ({ data: context }) => ({
+  class: context.pageClasses,
+  'data-build-time': context.buildTime,
+  'data-content-id': context.id,
+});
